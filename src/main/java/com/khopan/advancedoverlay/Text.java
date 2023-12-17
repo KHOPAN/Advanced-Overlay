75d8b92ae109807de7104cb46e88079a26e57c7d
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-17 16:43:47 +0700

    Add Advanced Overlay Config

--- src/main/java/com/khopan/advancedoverlay/Text.java
+++ src/main/java/com/khopan/advancedoverlay/Text.java
@@ -3,5 +3,5 @@ package com.khopan.advancedoverlay;
 import net.minecraft.network.chat.Component;
 
 public interface Text {
-	public static final Component CONFIG_SCREEN_TITLE = Component.translatableWithFallback("config.advancedoverlay.title", "Advanced Overlay Config");
+	public static final Component ADVANCED_OVERLAY_CONFIG = Component.translatableWithFallback("config.advancedoverlay.title", "Advanced Overlay Config");
 }
--- src/main/java/com/khopan/advancedoverlay/mixin/PauseScreenMixin.java
+++ src/main/java/com/khopan/advancedoverlay/mixin/PauseScreenMixin.java
@@ -8,7 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.khopan.advancedoverlay.Text;
+import com.khopan.advancedoverlay.screen.ChannelListScreen;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.layouts.GridLayout.RowHelper;
@@ -18,8 +20,6 @@ import net.minecraft.client.gui.screens.PauseScreen;
 public abstract class PauseScreenMixin {
 	@Inject(method="createPauseMenu()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/Minecraft;isLocalServer()Z", shift=Shift.BY, by=-1), locals=LocalCapture.CAPTURE_FAILSOFT)
 	public void onCreatePauseMenu(CallbackInfo info, GridLayout gridLayout, RowHelper rowHelper) {
-		rowHelper.addChild(Button.builder(Text.CONFIG_SCREEN_TITLE, button -> {
-
-		}).width(204).build(), 2);
+		rowHelper.addChild(Button.builder(Text.ADVANCED_OVERLAY_CONFIG, button -> Minecraft.getInstance().setScreen(new ChannelListScreen((PauseScreen) (Object) this))).width(204).build(), 2);
 	}
 }
