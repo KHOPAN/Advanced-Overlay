fabc3ea62824f3d236c891da647edbbf58c9d996
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-23 22:53:03 +0700

    Rendering Test

--- src/main/java/com/khopan/advancedoverlay/mixin/GuiMixin.java
+++ src/main/java/com/khopan/advancedoverlay/mixin/GuiMixin.java
@@ -6,6 +6,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.khopan.advancedoverlay.api.IModule;
+import com.khopan.advancedoverlay.builtin.helloworld.HelloWorldModule;
 import com.khopan.advancedoverlay.renderer.OverlayRenderer;
 import com.mojang.blaze3d.vertex.PoseStack;
 
@@ -15,6 +17,6 @@ import net.minecraft.client.gui.Gui;
 public abstract class GuiMixin {
 	@Inject(at=@At("TAIL"), method="render", slice=@Slice(from=@At(value="INVOKE", target="Lnet/minecraft/client/gui/components/PlayerTabOverlay;render(Lnet/mojang/blaze3d/vertex/PoseStack;ILnet/minecraft/world/scores/Scoreboard;Lnet/minecraft/world/scores/Objective;)V")))
 	public void render(PoseStack poseStack, float tickDelta, CallbackInfo info) {
-		OverlayRenderer.render(poseStack, tickDelta);
+		OverlayRenderer.render(poseStack, tickDelta, new IModule[] {new HelloWorldModule(), new HelloWorldModule()});
 	}
 }
--- src/main/java/com/khopan/advancedoverlay/renderer/OverlayRenderer.java
+++ src/main/java/com/khopan/advancedoverlay/renderer/OverlayRenderer.java
@@ -1,14 +1,41 @@
 package com.khopan.advancedoverlay.renderer;
 
+import com.khopan.advancedoverlay.api.IModule;
+import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 
 public class OverlayRenderer {
 	private OverlayRenderer() {}
 
-	public static void render(PoseStack stack, float tickDelta) {
+	public static void render(PoseStack stack, float tickDelta, IModule[] moduleList) {
+		if(moduleList == null || moduleList.length == 0) {
+			return;
+		}
+
 		Minecraft minecraft = Minecraft.getInstance();
-		minecraft.font.drawShadow(stack, "Hello, world!", 0.0f, 0.0f, 0xFFFFFF);
+		Window window = minecraft.getWindow();
+		int screenWidth = window.getGuiScaledWidth();
+		int screenHeight = window.getGuiScaledHeight();
+		int maxWidth = 0;
+		int totalHeight = 0;
+
+		for(int i = 0; i < moduleList.length; i++) {
+			int width = moduleList[i].getWidth();
+			maxWidth = Math.max(maxWidth, width);
+			totalHeight += moduleList[i].getHeight();
+		}
+
+		int x = screenWidth - maxWidth - 5;
+		int y = (int) Math.round((((double) screenHeight) - ((double) totalHeight)) * 0.5d);
+		Gui.fill(stack, x - 1, y - 1, x + maxWidth + 1, y + totalHeight + 1, 0x66000000);
+		
+		for(int i = 0; i < moduleList.length; i++) {
+			int height = moduleList[i].getHeight();
+			moduleList[i].render(stack, tickDelta, x, y, maxWidth, height);
+			y += height;
+		}
 	}
 }
